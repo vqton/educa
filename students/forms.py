01eba690733e8928f34ffe2dc9e63d3971b94453
vqton <vuquangton@outlook.com>
2022-05-15 12:25:14 +0700

    chua nhat

--- students/forms.py
+++ students/forms.py
@@ -1,16 +1,7 @@
 from django import forms
 from courses.models import Course
 
+
 class CourseEnrollForm(forms.Form):
-    course = forms.ModelChoiceField(queryset=Course.objects.all(), widget=forms.HiddenInput)
-
-
-class CourseEnrollForm(forms.ModelForm):
-    class Meta:
-        model = Course
-        fields = ['title', 'slug', 'overview']
-        widgets = {
-            'title': forms.TextInput(attrs={'class': 'form-control'}),
-            'slug': forms.TextInput(attrs={'class': 'form-control'}),
-            'overview': forms.Textarea(attrs={'class': 'form-control'}),
-        }
+    course = forms.ModelChoiceField(queryset=Course.objects.all(),
+                                    widget=forms.HiddenInput)
